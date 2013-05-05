fc7ee34677a0d34932d91fa336e04185b65161ec
Adam Chainz <adam@adamj.eu>
2013-05-05 17:56:58 +0100

    Moving factor finder, adding the ability to accept arguments from commandline

--- hopfield/factor_finder.py
+++ hopfield/factor_finder.py
@@ -7,7 +7,7 @@ from utils import random_binary_array
 
 class HopfieldFactorFinder(object):
 
-    def __init__(self, num_nodes, num_samples=10, start=10, stop=30, step=1):
+    def __init__(self, num_nodes, num_samples, start=10, stop=30, step=1):
         self.num_nodes = num_nodes
         self.num_samples = num_samples
 
@@ -36,7 +36,7 @@ class HopfieldFactorFinder(object):
         last = -1
         all_nums = []
         for percentage in xrange(self.start, self.stop + 1, self.step):
-            num = int((float(percentage)/100.0) * self.num_nodes)
+            num = max(1, int((float(percentage)/100.0) * self.num_nodes))
             if num > last:
                 all_nums.append(num)
                 last = num
@@ -96,4 +96,19 @@ class HopfieldFactorInspector(object):
 
 
 if __name__ == '__main__':
-    HopfieldFactorFinder(20).run()
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-n',
+                      '--num-nodes',
+                      dest='num_nodes',
+                      help='How many nodes to use in the hopfield network',
+                      type='int',
+                      default=20)
+    parser.add_option('-s',
+                      '--num-samples',
+                      dest='num_samples',
+                      help='How many times to sample each percentage',
+                      type='int',
+                      default=10)
+    (options, args) = parser.parse_args()
+    HopfieldFactorFinder(**options.__dict__).run()
